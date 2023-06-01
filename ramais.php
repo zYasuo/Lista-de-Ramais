e9f60606dd3438a247b220532bb0a27f5cf8c80f
Danilo S. Aguiar (1762) <daguiar@nrtelecom.com.br>
2023-06-01 10:11:37 -0300

    Arquivo Index

---
+++ ramais.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="ramaiscss.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <title>Lista de Ramais</title>
+</head>
+<body>
+<header>
+    </header>
+
+    <br><br>
+        <div id="search-container">
+            <h1>Lista de Ramais</h1>
+                <input type="text" id="searchInput" placeholder="Digite para buscar...">
+        </div>
+<script src="ramais.js"></script>
+</body>
+</html>
